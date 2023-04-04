a41a000c8b29e2303c97124b2557f74bb16988a2
BYRA Bastien <bbyra@u-bordeaux.fr>
2023-04-04 16:39:15 +0200

    ajout champ "code" à Reservation

---
+++ db/migrate/20230404143842_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.integer :seat_number
+      t.string :code
+      t.references :user, null: false, foreign_key: true
+      t.references :flight, null: false, foreign_key: true
+      t.references :seat_class_choice, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
